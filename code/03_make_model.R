389ae013466a2fe57754a8a14edcb8c996fb2edd
Grace Li <graceli@GracedeMacBook-Pro.local>
2025-04-04 01:14:20 -0400

    complete 03_make_model.R

---
+++ code/03_make_model.R
@@ -0,0 +1,27 @@
+here::i_am("code/03_make_model.R")
+
+data <- readRDS(
+  file = here::here("output/data_clean.rds")
+)
+
+library(gtsummary)
+
+data$diagnosis <- as.factor(data$diagnosis) 
+logit_model <- glm(diagnosis ~ radius_mean + perimeter_worst  + texture_mean + area_worst , 
+                   data = data, 
+                   family = binomial)
+
+regression_table <- logit_model %>%
+  tbl_regression(exponentiate = TRUE) %>%
+  add_global_p() %>% 
+  bold_labels()  
+
+saveRDS(
+  logit_model,
+  file = here::here("output/regression_models.rds")
+)
+
+saveRDS(
+  regression_table,
+  file = here::here("output/regression_tables.rds")
+)
